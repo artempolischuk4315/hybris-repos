59891db8534a4ab6d9dbd5c9f41793ca2882637e
Artem Polishchuk <art4315@gmail.com>
2020-08-28 15:29:47 +0300

    Custom ext fix

---
+++ extensions/myext/gensrc/org/training/constants/GeneratedMyextConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 28.08.2020 15:06:24                         ---
+ * ----------------------------------------------------------------
+ */
+package org.training.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedMyextConstants
+{
+	public static final String EXTENSIONNAME = "myext";
+	
+	protected GeneratedMyextConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
