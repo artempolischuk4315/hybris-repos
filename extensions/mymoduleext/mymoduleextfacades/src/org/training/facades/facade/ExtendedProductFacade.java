1e55fa325dabdf322bdb25bb76c9b9f037aacbf6
Artem Polischuk <art4315@gmail.com.com>
2020-09-14 23:01:19 +0300

    task5.4.3

---
+++ extensions/mymoduleext/mymoduleextfacades/src/org/training/facades/facade/ExtendedProductFacade.java
@@ -0,0 +1,7 @@
+package org.training.facades.facade;
+
+import de.hybris.platform.commercefacades.product.impl.DefaultProductVariantFacade;
+
+public class ExtendedProductFacade extends DefaultProductVariantFacade {
+
+}
