e5bad04bbdbb552d05b9c38ef730ee17018c5b17
Artem Polishchuk <art4315@gmail.com>
2020-09-18 18:04:36 +0300

    task8.2.1

---
+++ extensions/mymoduleext/mymoduleextcore/src/org/training/core/service/MyCustomerService.java
@@ -0,0 +1,7 @@
+package org.training.service;
+
+import de.hybris.platform.servicelayer.user.impl.DefaultUserService;
+
+public class MyCustomerService extends DefaultUserService {
+
+}
---
+++ extensions/mymoduleext/mymoduleextcore/src/org/training/core/workflow/SupervisorRegistrationConfirmationActionJob.java
@@ -0,0 +1,29 @@
+package org.training.workflow;
+
+import de.hybris.platform.core.model.user.CustomerModel;
+import de.hybris.platform.servicelayer.model.ModelService;
+import de.hybris.platform.workflow.model.WorkflowActionModel;
+import de.hybris.platform.workflow.model.WorkflowDecisionModel;
+import org.apache.log4j.Logger;
+
+public class SupervisorRegistrationConfirmationActionJob extends AbstractAdvancedUserRegistrationActionJob {
+    private static final Logger LOG = Logger.getLogger(SupervisorRegistrationConfirmationActionJob.class);
+
+    @Override
+    public WorkflowDecisionModel perform(final WorkflowActionModel action)
+    {
+        final CustomerModel customer = getAttachedCustomer(action);
+
+        LOG.info("Customer " + customer.getUid() + " confirmed by supervisor!");
+
+        customer.setLoginDisabled(false);
+        ModelService modelService = getModelService();
+        modelService.save(customer);
+
+        for (final WorkflowDecisionModel decision : action.getDecisions())
+        {
+            return decision;
+        }
+        return null;
+    }
+}
