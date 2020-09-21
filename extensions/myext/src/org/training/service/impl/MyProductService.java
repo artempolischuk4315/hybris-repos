06fea369766552a6248fb5350549fb1cea8087b8
Artem Polishchuk <art4315@gmail.com>
2020-09-21 23:31:01 +0300

    task8.8.1 fix

--- extensions/myext/src/org/training/service/impl/MyProductService.java
+++ extensions/myext/src/org/training/service/impl/MyProductService.java
@@ -1,2 +1,27 @@
-package org.training.service.impl;public class MyProductService {
+package org.training.service.impl;
+
+import de.hybris.platform.core.model.product.ProductModel;
+import de.hybris.platform.product.impl.DefaultProductService;
+import de.hybris.platform.servicelayer.model.ModelService;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+public class MyProductService extends DefaultProductService {
+
+    private ModelService modelService;
+
+    @Transactional
+    public void updateManufacturerNameForProducts(String manufacturerName, List<ProductModel> products) {
+        products.forEach(productModel -> {
+                    productModel.setManufacturerName(manufacturerName);
+                    modelService.save(productModel);
+        });
+    }
+
+    @Required
+    public void setModelService(ModelService modelService) {
+        this.modelService = modelService;
+    }
 }
